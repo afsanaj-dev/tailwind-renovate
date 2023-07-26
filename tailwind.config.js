80e08326686857e742033d834c0fbe49efbc9fac
afsana04 <129384941+afsana04@users.noreply.github.com>
2023-07-26 20:20:32 +0600

    tailwind-project-renovate

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        maxWidth: {
+            container: "1180px",
+          },
+        colors: {
+          clifford: '#da373d',
+        },
+        fontSize :{
+
+        },
+        variants: {
+
+          fontStyle: 'Dancing Script',
+          textColor: {
+              'primary': '#3490dc',
+                 'secondary': '#ffed4a',
+                 'danger': '#e3342f',
+              }
+
+        }
+         
+
+        // font-family: 'Dancing Script', cursive;
+      }
+    }
+  }
