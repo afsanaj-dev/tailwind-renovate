c9c8e9a19a63f3d0225be61b1928c358afc04496
afsanaj-dev <afsana2884@gmail.com>
2024-10-28 14:28:54 +0600

    nav-menu z-index modified
    nav menu z-index increased and hover color added to nav list

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,23 @@
 tailwind.config = {
-    theme: {
-      extend: {
-        maxWidth: {
-            container: "1180px",
-          },
-        colors: {
-          clifford: '#da373d',
+  theme: {
+    extend: {
+      maxWidth: {
+        container: "1180px",
+      },
+      colors: {
+        clifford: "#da373d",
+      },
+      fontSize: {},
+      variants: {
+        fontStyle: "Dancing Script",
+        textColor: {
+          primary: "#3490dc",
+          secondary: "#ffed4a",
+          danger: "#e3342f",
         },
-        fontSize :{
+      },
 
-        },
-        variants: {
-
-          fontStyle: 'Dancing Script',
-          textColor: {
-              'primary': '#3490dc',
-                 'secondary': '#ffed4a',
-                 'danger': '#e3342f',
-              }
-
-        }
-         
-
-        // font-family: 'Dancing Script', cursive;
-      }
-    }
-  }
+      // font-family: 'Dancing Script', cursive;
+    },
+  },
+};
